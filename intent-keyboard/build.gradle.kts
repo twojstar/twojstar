028d2f7b07f4458f2bc25b31b9f1e3e795722588
trvny <120686325+trvny@users.noreply.github.com>
2026-09-08 11:06:10 +0200

    build(intent-keyboard): resolve Kotlin compiler override

--- intent-keyboard/build.gradle.kts
+++ intent-keyboard/build.gradle.kts
@@ -1,6 +1,10 @@
 buildscript {
     val kotlinVersion = providers.gradleProperty("kotlinVersion").get()
 
+    repositories {
+        mavenCentral()
+    }
+
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
