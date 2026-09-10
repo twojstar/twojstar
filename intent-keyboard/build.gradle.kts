c547e75237c085463b616d73994a3b2a93aa599d
trvny <120686325+trvny@users.noreply.github.com>
2026-09-10 22:37:10 +0200

    build(intent-keyboard): share Android plugin classloader

--- intent-keyboard/build.gradle.kts
+++ intent-keyboard/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.multiplatform") apply false
     id("org.jetbrains.kotlin.jvm") apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
 }
